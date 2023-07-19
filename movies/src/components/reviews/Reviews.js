cf2471e2a8008c267662f7f3298f42ba8717bc87
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2023-07-19 23:15:50 +0100

    Added react front end

---
+++ movies/src/components/reviews/Reviews.js
@@ -0,0 +1,92 @@
+import {useEffect, useRef} from 'react';
+import api from '../../api/axiosConfig';
+import {useParams} from 'react-router-dom';
+import {Container, Row, Col} from 'react-bootstrap';
+import ReviewForm from '../reviewForm/ReviewForm';
+
+import React from 'react'
+
+const Reviews = ({getMovieData,movie,reviews,setReviews}) => {
+
+    const revText = useRef();
+    let params = useParams();
+    const movieId = params.movieId;
+
+    useEffect(()=>{
+        getMovieData(movieId);
+    },[])
+
+    const addReview = async (e) =>{
+        e.preventDefault();
+
+        const rev = revText.current;
+
+        try
+        {
+            const response = await api.post("/api/reviews", {reviewBody:rev.value,imdbId:movieId});
+
+            const updatedReviews = [...reviews, {body:rev.value}];
+    
+            rev.value = "";
+    
+            setReviews(updatedReviews);
+        }
+        catch(err)
+        {
+            console.error(err);
+        }
+
+    }
+
+  return (
+    <Container>
+        <Row className="my-3">
+            <Col><h3>Reviews</h3></Col>
+        </Row>
+        <Row>
+            <Col>
+                <img src={movie?.poster} alt="" />
+            </Col>
+            <Col>
+                {
+                    <>
+                        <Row>
+                            <Col>
+                                <ReviewForm handleSubmit={addReview} revText={revText} labelText = "Write a Review?" />  
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <hr />
+                            </Col>
+                        </Row>
+                    </>
+                }
+                {
+                    reviews?.map((r) => {
+                        return(
+                            <>
+                                <Row>
+                                    <Col>{r.body}</Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <hr />
+                                    </Col>
+                                </Row>                                
+                            </>
+                        )
+                    })
+                }
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <hr />
+            </Col>
+        </Row>        
+    </Container>
+  )
+}
+
+export default Reviews
--- src/main/java/dev/movies/Movies/ReviewController.java
+++ src/main/java/dev/movies/Movies/ReviewController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/reviews")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ReviewController {
     private ReviewService reviewService;
     @PostMapping
